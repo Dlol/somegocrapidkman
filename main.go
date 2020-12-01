42e7f1cd2b80443229a023eaa36a4dc077aaac6c
Dlol Nadas <dravennadas@gmail.com>
2020-11-30 18:17:32 -0600

    wow

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	integer := 0
-	var name string
-	fmt.Println("\n----------\nhehehhee\n----------")
-	for i := 0; i < 8; i++ {
-		fmt.Println("x:", integer)
-		increment(&integer)
-	}
-	fmt.Println("ok gimme ur first name i promise i wont steal ur identity")
-	fmt.Scanln(&name)
-	fmt.Println("HAHA, ", name, " I HAVE STOLEN UR IDENTITITYTYI!")
-}
-
-func increment(x *int) {
-	*x++
+	// guessing game time??
 }
